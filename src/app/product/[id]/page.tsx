0c456e27529ad871bb7ec5b4e94412c9560154a6
artemriabyi6 <ar04082001@gmail,com>
2025-08-11 19:36:04 +0300

    trying to fix product page 2

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,35 +1,35 @@
-import { notFound } from 'next/navigation';
-import ProductDetails from '../../../components/ProductDetails';
-import { products } from '../../../data/products';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+// import { notFound } from 'next/navigation';
+// import ProductDetails from '../../../components/ProductDetails';
+// import { products } from '../../../data/products';
+// import Header from '@/components/Header';
+// import Footer from '@/components/Footer';
 
-interface ProductPageProps {
-  params: {
-    id: string;
-  };
-}
+// interface ProductPageProps {
+//   params: {
+//     id: string;
+//   };
+// }
 
-export default function ProductPage({ params }: ProductPageProps) {
-  const productId = parseInt(params.id);
+// export default function ProductPage({ params }: ProductPageProps) {
+//   const productId = parseInt(params.id);
   
-  if (isNaN(productId)) {
-    return notFound();
-  }
+//   if (isNaN(productId)) {
+//     return notFound();
+//   }
 
-  const product = products.find(product => product.id === productId);
+//   const product = products.find(product => product.id === productId);
   
-  if (!product) {
-    return notFound();
-  }
+//   if (!product) {
+//     return notFound();
+//   }
 
-  return (
-    <>
-      <Header />
-      <main className="container">
-        <ProductDetails product={product} />
-      </main>
-      <Footer />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Header />
+//       <main className="container">
+//         <ProductDetails product={product} />
+//       </main>
+//       <Footer />
+//     </>
+//   );
+// }
